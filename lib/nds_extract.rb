4e7fc3c69453e2b85f90b97e7ef3cdf4847d1ff1
Jeremy Garnes <j06garnes@gmail.com>
2020-01-09 05:33:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,7 +40,8 @@ def total_gross(source)
   grand_total = 0  
   row_index = 0 
   while row_index < list_of_directors(source).length do
-   grand_total += list_of_directors(source)[row_index]
+   grand_total += directors_totals(source)
+   binding.pry 
    row_index += 1 
 
    grand_total
